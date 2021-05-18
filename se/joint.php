0bbc77f2ed7c1a39abf908f5cd208635f6d26cf5
Pale0059 <79786360+Pale0059@users.noreply.github.com>
2021-05-18 17:06:43 +0700

    Delete se directory

--- se/joint.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-        $idInput = $_GET['id'];      
-        $_SESSION["positId"]  = $idInput;
-        Header("refresh:0;url=edituser.php");
-        die();
-        
-?>
-
--- se/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-?>
-<!-- ////////////ออก////////////// -->
-<?php
-session_destroy();
-Header("refresh:0;url=home.php");
-?>
